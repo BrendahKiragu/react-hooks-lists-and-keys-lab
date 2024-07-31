6f930ab9a36c43a23d6e314e47423d5c56b1b278
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-31 15:51:02 +0300

    adds aligorithm as comments

--- src/components/App.js
+++ src/components/App.js
@@ -17,3 +17,10 @@ function App() {
 }
 
 export default App;
+//user has:name,city,bio,color,links,projects
+
+//our app is the parent component
+//it renders navbar, home, about,projectlist components (sections)
+//we do import of these components
+//we also import user data from a user component that is in another folder
+//user data has a project array that has objects with  4 properties
